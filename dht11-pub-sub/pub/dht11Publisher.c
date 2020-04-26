5b221a0fd6713eede7527f6cc54f2f535503b11a
Tanuj Sharma <asktanuj@gmail.com>
2020-04-26 21:03:34 +0530

    Enabled RaspberryPi

--- dht11-pub-sub/pub/dht11Publisher.c
+++ dht11-pub-sub/pub/dht11Publisher.c
@@ -25,7 +25,7 @@
 * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 * POSSIBILITY OF SUCH DAMAGE.
 ******************************************************************************/
-
+#define RASPBERRY_PI  1
 #if RASPBERRY_PI
 #include <wiringPi.h>
 #endif
